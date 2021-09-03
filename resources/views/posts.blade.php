8699a15ccd015bf7aa17dae5338185ded301f118
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-03 20:13:09 +0300

    section4 working with DB - Your First Eloquent Relationship - lesson 24

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,12 +3,13 @@
         @foreach($posts as $post)
 
             <article class="{{ $loop->even ? 'dor' : '' }}">
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
+
+                <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }} </a></h1>
+
+                <p><a href="#">{{ $post->category->name }}</a></p>
+
                 <div> {{ $post->excerpt }}</h1>
+                    
             </article>
             
         @endforeach

385247487eaafd5b8ad155cbc3e123e10f8eb434
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-04 16:26:38 +0300

    section 4 working with DB - Eager Load Relationships on an Existing Model - lesson 30

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
+
+    protected $with = ['category', 'author'];
 
     public function category(){
         return $this->belongsTo(Category::class);

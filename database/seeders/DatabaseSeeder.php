9839beedf83f33e78f3baa5744984e3ed66667d0
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-04 14:36:48 +0300

    section 4 working with DB - Turbo Boost With Factories - lesson 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Category;
 use App\Models\Post;
+
 use Illuminate\Database\Seeder;
 
 
@@ -16,48 +16,16 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+    public function run(){
 
-        $user = User::factory()->create();
+        $user = User::factory()->create([
 
-        $personal = Category::create([
-            'name' => 'Presonal',
-            'slug' => 'personal'  
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family' 
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'  
-        ]);
-
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet purus id nunc accumsan, eleifend dictum metus imperdiet. ',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet purus id nunc accumsan, eleifend dictum metus imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sit amet dui et lacus elementum scelerisque non eu dui. Sed vehicula vehicula elit nec convallis. Mauris imperdiet sem vel dolor eleifend, et ornare dolor maximus. Etiam ligula urna, ullamcorper sed accumsan sit amet, venenatis vitae ipsum.'
-        ]);
-        
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet purus id nunc accumsan, eleifend dictum metus imperdiet. ',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet purus id nunc accumsan, eleifend dictum metus imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sit amet dui et lacus elementum scelerisque non eu dui. Sed vehicula vehicula elit nec convallis. Mauris imperdiet sem vel dolor eleifend, et ornare dolor maximus. Etiam ligula urna, ullamcorper sed accumsan sit amet, venenatis vitae ipsum.'
-        ]);
-
     }
 
 }

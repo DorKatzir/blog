2c7744a3673c28dc2a5f4cdff9c7b029320d2fab
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-04 22:43:50 +0300

    section 5 Integrate Design - Convert the HTML and CSS to Blade - Lesson 31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,43 @@
 <x-layout>
-     
-        @foreach($posts as $post)
+    
+    {{-- @foreach($posts as $post)
 
-            <article>
+        <article>
 
-                <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }} </a></h1>
+            <h1><a href="/posts/{{ $post->slug }}"> {{ $post->title }} </a></h1>
 
-                <p>
-                    By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-                    <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-                </p>
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
 
-                <div> {{ $post->excerpt }}</h1>
+            <div> {{ $post->excerpt }}</h1>
 
-            </article>
-            
-        @endforeach
-     
+        </article>
+        
+    @endforeach --}}
+
+
+
+    @include('_posts-header');
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+    </main>
+
 </x-layout>
 
 

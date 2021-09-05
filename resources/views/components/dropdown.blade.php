9a0df16e33872a5a7511a6d2511380267c419dd8
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-06 02:36:29 +0300

    section 5 Integrate Design - Quick Tweaks and Clean-Up - Lesson 36

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     {{-- Links --}}
-    <div x-show="show" class="py-2 absolute font-semibold bg-gray-100 mt-2 rounded-xl z-50 min-w-full w-max" style="display:none">
+    <div x-show="show" class="py-2 absolute font-semibold bg-gray-100 mt-2 rounded-xl z-50 min-w-full w-max overflow-auto max-h-52 dor-scrollbar" style="display:none">
         {{ $slot }}
     </div>
 

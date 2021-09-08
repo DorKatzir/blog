6cfff02ceca4b89de48346c4b8aabb246cab0c95
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-09 01:12:44 +0300

    section 5 Integrate Design - Search - Lesson 37 38

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,13 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
 
     protected $guarded = [];
 
     protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters){
+
+        $query->when($filters['search'] ?? false, function($query, $search){
+
+            $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%');
+        });  
+
+    }
 
     public function category(){
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
@@ -16,22 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [ PostController::class, 'index' ] )->name('home');
 
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-
-
-Route::get('posts/{post:slug}', function(Post $post){
-
-   return view('post', [
-       'post' =>  $post
-   ]);    
-});
+Route::get('posts/{post:slug}', [ PostController::class, 'show' ]);
 
 
 

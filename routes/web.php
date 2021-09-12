1315af4c20a2161b0244ab992b1fc935e4e23d39
DorKatzir <DorKatzir@users.noreply.github.com>
2021-09-12 18:25:40 +0300

    Section 7 - Filtering - lesson 39

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
+
 use App\Http\Controllers\PostController;
-use App\Models\User;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [ PostController::class, 'index' ] )->name('home');
 
@@ -21,12 +12,3 @@ Route::get('posts/{post:slug}', [ PostController::class, 'show' ]);
 
 
 
-
-Route::get('authors/{author:username}', function(User $author){
-
-    return view('posts', [
-        'posts' =>  $author->posts,
-        'categories' => Category::all()
-    ]);
-    
-})->name('author');
